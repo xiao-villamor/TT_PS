8b7054981d9b6e3d8268d76cc06d6db14b2f5c75
Xiao Villamor <xiaovilamor@gmail.com>
2022-10-15 11:12:05 +0200

    Initial commit

---
+++ app/src/main/java/es/udc/psi/tt_ps/MainActivity.java
@@ -0,0 +1,14 @@
+package es.udc.psi.tt_ps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
