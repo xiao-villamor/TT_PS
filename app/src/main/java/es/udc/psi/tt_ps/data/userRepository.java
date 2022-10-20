010c97a0b8707a28887cd949d1888d839960171c
Xiao Villamor <xiaovilamor@gmail.com>
2022-10-20 16:12:33 +0200

    feature : add deleteUser

--- app/src/main/java/es/udc/psi/tt_ps/data/userRepository.java
+++ app/src/main/java/es/udc/psi/tt_ps/data/userRepository.java
@@ -21,8 +21,8 @@ import java.util.concurrent.Executor;
 import es.udc.psi.tt_ps.data.model.UserModel;
 
 public class userRepository {
-    private FirebaseAuth mAuth;
-    private FirebaseFirestore db;
+    private final FirebaseAuth mAuth;
+    private final FirebaseFirestore db;
 
     public userRepository(FirebaseAuth mAuth, FirebaseFirestore db) {
         this.mAuth = mAuth;
@@ -30,13 +30,11 @@ public class userRepository {
     }
 
     public void createUser(UserModel user){
-        mAuth = FirebaseAuth.getInstance();
-
+        Log.d("TAG","create start");
         mAuth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword()).addOnCompleteListener( task -> {
             if (task.isSuccessful()) {
                 // Sign in success, update UI with the signed-in user's information
                 Log.d("TAG", "signInWithEmail:success " + task.getResult().getUser() );
-                mAuth.updateCurrentUser(Objects.requireNonNull(task.getResult().getUser()));
                 db.collection("User_Info").document(task.getResult().getUser().getUid()).set(user);
 
             } else {
@@ -47,8 +45,13 @@ public class userRepository {
         });
     }
 
-    public void updateUser( UserModel user) {
-        db.collection("User_Info").document(mAuth.getCurrentUser().getUid()).set(user);
+    public void updateUser(UserModel user) {
+        db.collection("User_Info").document(mAuth.getUid()).set(user);
+    }
+
+    public void deleteUser() {
+        db.collection("User_Info").document(mAuth.getUid()).delete();
+        mAuth.getCurrentUser().delete();
     }
 
 
--- app/src/main/java/es/udc/psi/tt_ps/ui/view/MainActivity.java
+++ app/src/main/java/es/udc/psi/tt_ps/ui/view/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.sql.Date;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 import es.udc.psi.tt_ps.R;
 import es.udc.psi.tt_ps.data.model.UserModel;
@@ -20,7 +21,8 @@ import es.udc.psi.tt_ps.data.userRepository;
 import es.udc.psi.tt_ps.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,23 +31,22 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         FirebaseApp.initializeApp(this);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
 
         userRepository r;
         r = new userRepository(mAuth,db);
         UserModel u;
-
         u = new UserModel("pakirrin1234", "Pepe", "Perez", Date.valueOf("1999-01-01"), "2313dasda@gmail.com",
                 "666666666", "profilePic", null, null, null);
 
         r.createUser(u);
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        binding.textView.setText(db.collection("User_Info").document(currentUser.getUid()).toString());
-        UserModel c = new UserModel("pakirrin1234", "edit", "Perez", Date.valueOf("1999-01-01"), "mailRand@gmail.com",
-        "1312", "profilePic", null, null, null);
-        r.updateUser(c);
+
+        binding.button.setOnClickListener(v -> {
+            r.deleteUser();
+        });
 
     }
+
 }
